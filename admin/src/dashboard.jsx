e15dba66b759e029b9308f5f5e317e6e3c052a80
princebusa <busaprince13@gmail.com>
2025-07-06 17:12:50 +0530

    error fix

--- admin/src/dashboard.jsx
+++ admin/src/dashboard.jsx
@@ -5,14 +5,12 @@ import Reports from './pages/dashboard/Reports'
 import Analytics from './pages/dashboard/Analytics'
 import Inbox from './pages/dashboard/inbox'
 import FolderPage from './pages/dashboard/folderPage';
-import Test from './pages/dashboard/Test';
 import Singleproject from './pages/dashboard/singleProject'
 const DashboardRoutes = () => {
   return (
     <Routes>
        <Route path="reports/files/:foldername" element={<FolderPage />} />
        <Route path="project/:projectname" element={<Singleproject/>} />
-       <Route path="test" element={<Test/>}/>
       <Route path="home" element={<Dashboard />} />
       <Route path="inbox" element={<Inbox />} />
       <Route path="projects" element={<Plans />} />
