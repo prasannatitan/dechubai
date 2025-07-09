4cecf14f2d1627f8379cb830035281f10afe2721
princebusa <busaprince13@gmail.com>
2025-07-09 15:15:24 +0530

    import fix

--- backend/routes/projectData.js
+++ backend/routes/projectData.js
@@ -1,7 +1,7 @@
 const admin = require("firebase-admin");
 const express = require("express");
 const router = express.Router();
-const authMiddlware = require("../middleware/AuthMiddleware");
+const authMiddlware = require("../Middleware/AuthMiddleware");
 const taskModel = require("../models/taskModel");
 
 
