c9f0de8966db52b341d4c5eb6897e1e0f6459847
princebusa <busaprince13@gmail.com>
2025-08-27 12:24:56 +0530

    backend fix

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-    username: { type: String, required: true, unique: true },
+    username: { type: String, required: true},
     email: { type: String, required: true, unique: true },
     password: { type: String},
     googleId: { type: String},
